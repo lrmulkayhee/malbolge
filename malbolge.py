73282e074b7c6350b7a078a04adbb6dd120e1f7c
Lauren Mulcahy <lxmulcahy@gmail.com>
2024-09-14 18:43:32 -0400

    Refactor Malbolge interpreter implementation

--- malbolge.py
+++ malbolge.py
@@ -1,14 +1,14 @@
-# malbolge.py
 import sys
 
-def rotate_char(c):
-    return chr((ord(c) + 1) % 256)
+def crazy_op(a, b):
+    return ((a // 81) % 3) * 81 + ((a // 9) % 9) * 9 + ((a // 1) % 9)
 
 def malbolge_interpreter(code):
     memory = [0] * 59049
     for i in range(len(code)):
         memory[i] = ord(code[i])
     a = c = d = 0
+    print("Starting interpretation...")  # Debug print
     while c < len(code):
         d = memory[c]
         if d < 33 or d > 126:
@@ -16,7 +16,7 @@ def malbolge_interpreter(code):
         if d == 106:
             a = memory[a]
         elif d == 109:
-            memory[a] = rotate_char(memory[a])
+            memory[a] = (memory[a] + 1) % 256
         elif d == 110:
             a = (a + 1) % 59049
         elif d == 112:
@@ -26,10 +26,11 @@ def malbolge_interpreter(code):
         elif d == 114:
             print(chr(memory[a]), end='')
         c += 1
+    print("\nInterpretation finished.")  # Debug print
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python malbolge.py <filename>")
+        print("Usage: python3 malbolge_interpreter.py <filename>")
         sys.exit(1)
     with open(sys.argv[1], 'r') as f:
         code = f.read()
